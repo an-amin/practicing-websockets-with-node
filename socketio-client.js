2f9863913cc3425445d0ff70bc5ba9d9583f868c
Aminul Islam <amin@ba-ts.com>
2022-02-13 14:01:10 +0600

    first commit

---
+++ socketio-client.js
@@ -0,0 +1,11 @@
+const io = require("socket.io-client");
+
+const socket = io("ws://example.com/my-namespace", {
+  reconnectionDelayMax: 10000,
+  auth: {
+    token: "123"
+  },
+  query: {
+    "my-key": "my-value"
+  }
+});
